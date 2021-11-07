2f70265a6725a7acf382deb599bae78a25c7d6de
ADMIN <artyom.pashun@gmail.com>
2021-11-07 20:37:19 +0300

    Task 4

---
+++ src/by/epamtc/pashunArtyom/digitalLibrary/controller/Commands.java
@@ -0,0 +1,31 @@
+package by.epamtc.pashunArtyom.digitalLibrary.controller;
+
+import by.epamtc.pashunArtyom.digitalLibrary.controller.command.Command;
+import by.epamtc.pashunArtyom.digitalLibrary.controller.command.CommandName;
+import by.epamtc.pashunArtyom.digitalLibrary.controller.command.impl.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Commands {
+    private static final Map<CommandName, Command> commands = new HashMap<>();
+
+    public Commands (){
+        commands.put(CommandName.ADD_BOOK, new AddBook());
+        commands.put(CommandName.EDIT_BOOK, new EditBook());
+        commands.put(CommandName.FIND_BOOK, new FindBook());
+        commands.put(CommandName.REGISTER, new Register());
+        commands.put(CommandName.SIGN_IN, new SignIn());
+    }
+
+    public Command getCommand(String request) {
+        CommandName actions = CommandName.(name);
+        Command command;
+        if (actions == null) {
+            command = commands.get(Actions.WRONG_REQUEST);
+        } else {
+            command = commands.get(actions);
+        }
+        return command;
+    }
+}
--- src/by/epamtc/pashunArtyom/digitalLibrary/controller/command/impl/AddBook.java
+++ src/by/epamtc/pashunArtyom/digitalLibrary/controller/command/impl/AddBook.java
@@ -2,14 +2,31 @@ package by.epamtc.pashunArtyom.digitalLibrary.controller.command.impl;
 
 import by.epamtc.pashunArtyom.digitalLibrary.controller.command.Command;
 import by.epamtc.pashunArtyom.digitalLibrary.controller.exception.ControllerException;
+import by.epamtc.pashunArtyom.digitalLibrary.entity.Book;
 import by.epamtc.pashunArtyom.digitalLibrary.service.ServiceFactory;
+import by.epamtc.pashunArtyom.digitalLibrary.service.exception.ServiceException;
 import by.epamtc.pashunArtyom.digitalLibrary.service.impl.TXTLibService;
 
 public class AddBook implements Command {
+
     @Override
     public String toDo(String request) throws ControllerException {
-        ServiceFactory factory = ServiceFactory.getServiceLink();
-        TXTLibService bookService = factory.getBookService();
-        return bookService.addBook();
+
+        String response = null;
+
+        String[] temp = request.split("; ");
+        Book newBook = new Book();
+        newBook.setBookId(1);
+        newBook.setBookTitle(temp[0]);
+        newBook.setAuthorName(temp[1]);
+        try {
+            ServiceFactory.getServiceLink().getBookService().addBook(newBook);
+            response = "Book has been added to library";
+        } catch (ArrayIndexOutOfBoundsException | ServiceException e) {
+            throw new ControllerException("Can't add a book");
+        }
+        //System.out.println(newBook.toString());
+
+        return response;
     }
 }
