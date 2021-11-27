f786443aac83829943748cc920ecfa06eefce9b3
ADMIN <artyom.pashun@gmail.com>
2021-11-27 15:50:08 +0300

    Task 4

--- src/by/epamtc/pashunArtyom/digitalLibrary/controller/command/CommandName.java
+++ src/by/epamtc/pashunArtyom/digitalLibrary/controller/command/CommandName.java
@@ -3,17 +3,11 @@ package by.epamtc.pashunArtyom.digitalLibrary.controller.command;
 public enum CommandName {
     SIGN_IN,
     REGISTER,
-    ADD_BOOK,
-    FIND_BOOK,
-    WRONG_REQUEST;
+    DELETE,
 
-    public static CommandName commandName(String request) {
-        CommandName command;
-        try {
-            command = CommandName.valueOf(request.toUpperCase());
-        } catch (IllegalArgumentException | NullPointerException e) {
-            command = CommandName.WRONG_REQUEST;
-        }
-        return command;
-    }
+    ADD_BOOK,
+    REMOVE_BOOK,
+    EDIT_BOOK,
+    FIND_BOOK,
+    SHOW_BOOKS,
 }
